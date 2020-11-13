d3dd2bc7f36f1ed9c42f02c8eec8d7dfd6752128
skumar6 <skumar6@empoweredbenefits.com>
2020-11-13 14:47:51 +0530

    error massage

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,15 +4,28 @@ import QuizQuestionButton from "./QuizQuestionButton";
 class QuizQuestion extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+        incorrectAnswer: false
+
+    }
   }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+        this.setState({
+            incorrectAnswer: false
+        })
       this.props.showNextQuestionHandler();
+    }else {
+        this.setState({
+            incorrectAnswer: true
+        })
     }
   }
   render() {
     return (
       <main>
+          {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
