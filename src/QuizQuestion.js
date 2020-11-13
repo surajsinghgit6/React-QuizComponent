9fd5f80563d0f29a7229f30b1ac611d78b61fbb2
skumar6 <skumar6@empoweredbenefits.com>
2020-11-13 14:37:38 +0530

    Handling event

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,6 +5,11 @@ class QuizQuestion extends Component {
   constructor(props) {
     super(props);
   }
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
+  }
   render() {
     return (
       <main>
@@ -13,7 +18,17 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => {
+                return (
+                  <QuizQuestionButton
+                    clickHandler={this.handleClick.bind(this)}
+                    key={index}
+                    button_text={answer_option}
+                  />
+                );
+              }
+            )}
           </ul>
         </section>
       </main>
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,10 +4,15 @@ class QuizQuestionButton extends Component {
   constructor(props) {
     super(props);
   }
+  handleClick() {
+    this.props.clickHandler(this.props.button_text);
+  }
   render() {
     return (
       <li>
-        <button>{this.props.button_text}</button>
+        <button onClick={this.handleClick.bind(this)}>
+          {this.props.button_text}
+        </button>
       </li>
     );
   }
